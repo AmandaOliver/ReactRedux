8bf3ac3067753c9c7f5ce691d8bac59b01c7d9cd
Amanda Oliver <amoliver@whm5004520.ds.williamhill.co.uk>
2017-12-13 17:17:40 +0100

    second commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+// Functional component
+
+// const SearchBar = () => {
+//     return <input />
+// }
+
+//Class component
+
+class SearchBar extends Component {
+    render() {
+        return <input />
+    }
+}
+
+export default SearchBar;
