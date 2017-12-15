ebb134884e18fd28ec68872d1e41813da54f0def
Amanda Oliver <amanda.oliver@williamhill.com>
2017-12-15 18:11:24 +0100

    finished lesson 2

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,8 +9,27 @@ import React, {Component} from 'react';
 //Class component
 
 class SearchBar extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = { term: '' };
+        
+    }
+
     render() {
-        return <input />
+        return (
+        <div className="search-bar">
+            <input 
+                value = {this.state.term}
+                onChange={event => this.onInputChange(event.target.value)} />
+        </div>
+        );
+    }
+
+    onInputChange(term){
+        this.setState({ term });
+        this.props.onSearchTermChange(term);
+
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
-
+import VideoDetails from './components/video_detail';
+import _ from 'lodash';
 const API_KEY = 'AIzaSyDd_uzFEvACGjEV7nn1FWi0k3Fp7NQulrg';
+var AWS = require('aws-sdk/dist/aws-sdk-react-native');
 
-//Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+            videos: [],
+            selectedVideo: null    
+        };
+    this.videoSearch('william hill');        
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, videos => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {
+            this.videoSearch(term);
+            console.log(new Date())
+        }, 500);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetails video={this.state.selectedVideo} />
+                <VideoList 
+                    videos={this.state.videos}/>
+            </div>
+        );
+    } 
+
 }
 
-//Take this component's generated HTML and put it on the page (in the DOM).
 ReactDOM.render(<App/>, document.querySelector('.container'));
