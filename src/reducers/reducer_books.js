a498810341db451260efdf84f3cfce5f47eb37bd
Tomasz Rekawek <tomasz.rekawek@williamhill.com>
2017-12-23 19:12:52 +0100

    final commit

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {title: 'JavaScript: The Good Parts'},
-        {title: 'Harry Potter and the Secret Chamber'},
-        {title: 'The Dark Tower'},
-        {title: 'Eloquent Ruby'}
+        {title: 'JavaScript: The Good Parts', pages: 101},
+        {title: 'Harry Potter and the Secret Chamber', pages: 85},
+        {title: 'The Dark Tower', pages: 50},
+        {title: 'Eloquent Ruby', pages: 1}
     ]
 }
