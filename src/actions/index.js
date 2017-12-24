95750524906843b633e861bb1a28a65cbfb4a574
Tomasz Rekawek <tomasz.rekawek@williamhill.com>
2017-12-24 15:38:34 +0100

    forecast finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
-vdfs
+import axios from 'axios';
+
+const API_KEY = '3517f66da58ce90c6698f6ba16751411';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` ;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(cityName){
+    const url = `${ROOT_URL}&q=${cityName},pl`;
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
